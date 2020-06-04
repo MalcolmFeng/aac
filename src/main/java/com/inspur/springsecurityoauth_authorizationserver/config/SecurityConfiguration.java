753e555d8735a775f486747ed4c2cd79e1363854
fengdianlong <fengdianlong@inspur.com>
2020-06-04 14:29:34 +0800

    登陆页面，登录失败页面，授权页面的编写；

--- src/main/java/com/inspur/springsecurityoauth_authorizationserver/config/SecurityConfiguration.java
+++ src/main/java/com/inspur/springsecurityoauth_authorizationserver/config/SecurityConfiguration.java
@@ -1,17 +1,31 @@
 package com.inspur.springsecurityoauth_authorizationserver.config;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.*;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -42,14 +56,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatchers()
-                .antMatchers("/login", "/login-error", "/oauth/authorize", "/oauth/token")
-                .and().authorizeRequests()
-                .antMatchers("/login").permitAll().anyRequest().authenticated();
-        // 登录页面
-        http.formLogin().loginPage("/login").failureUrl("/login-error");
-        // 禁用CSRF
-        http.csrf().disable();
+        http.authorizeRequests()
+                // 配置需要角色权限的地址
+//                .antMatchers("/admin/**").hasRole("admin")
+//                .antMatchers("/db/**").hasAnyRole("admin","user")
+//                .antMatchers("/user/**").access("hasAnyRole('admin','user')")
+                // 配置不需要认证的地址
+                .antMatchers("/login", "/login-error", "/oauth/authorize", "/oauth/token").permitAll()
+                // 配置只需要认证的地址
+                .anyRequest().authenticated()
+                // 配置登录地址
+                .and().formLogin()
+                    .loginPage("/login")
+                    .failureUrl("/login-error")
+                    .permitAll().and()
+                .csrf().disable();
     }
 
     @Autowired
